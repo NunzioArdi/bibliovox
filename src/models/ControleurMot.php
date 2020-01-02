d603d5a8fd404723a21b92bbdaf4072501ebb987
Paul Claude <paul.claude7@etu.univ-lorraine.fr>
2020-01-02 23:12:17 +0100

    Accès aux détails d'un mot avec image et audio

--- src/models/ControleurMot.php
+++ src/models/ControleurMot.php
@@ -6,19 +6,16 @@ namespace bibliovox\models;
 
 class ControleurMot
 {
-    static function renderMot(Mot $mot){
-echo "<div class = 'mot'>";
-echo "<h1>$mot->texte</h1>";
+    static function renderMot(Mot $mot)
+    {
+        echo "<div class = 'mot'>";
+        echo "<h1>$mot->texte</h1>";
 
-echo "<img src='" . PATH . "/media/img/img/" . $mot->image ."' alt='img'>";
+        echo "<img src='" . PATH . "/media/img/img/" . $mot->image . "' alt='img'>";
 
-echo "<br><audio controls>";
-echo "<source src='" . PATH. "/media/aud/dico/" . $mot->audio ."' type='audio/mp3'>";
-echo "</audio></div>";
-
-
-
-
+        echo "<br><audio controls>";
+        echo "<source src='" . PATH . "/media/aud/dico/" . $mot->audio . "' type='audio/mp3'>";
+        echo "</audio></div>";
     }
 
 }
