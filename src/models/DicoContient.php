60337b4115610a1ec8455f3a2aa7b0c0dd78e810
BERNARD Hugo <hugobernard39@gmail.com>
2020-01-28 19:41:49 +0100

    Résolution conflit

--- src/models/DicoContient.php
+++ src/models/DicoContient.php
@@ -14,7 +14,18 @@ class DicoContient extends Model
 {
     protected $table = 'dicoContient';
 
-    protected static function motContenuDico(int $idDico) {
+    protected static function motContenuDico(int $idDico)
+    {
         return DicoContient::where('idD', '=', "$idDico")->get();
+    }
+
+    public static function matchIDs(int $idMot, int $idDico): bool
+    {
+        if ($idDico == -1) {
+            return true;
+        }
+        else {
+            return DicoContient::where('idD', '=', "$idDico")->where("idM", "=", "$idMot")->first() != null;
+        }
     }
 }
