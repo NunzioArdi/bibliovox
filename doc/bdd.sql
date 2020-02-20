30662a3d3ac4ae9d89a6ba2c44e65b74931de9f2
Hugo Bernard <hugobernard39@gmail.com>
2020-02-20 22:22:58 +0100

    Mise à jour de la base de donnée

--- doc/bdd.sql
+++ doc/bdd.sql
@@ -2,45 +2,65 @@
 -- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Jan 09, 2020 at 05:14 PM
--- Server version: 5.7.26
--- PHP Version: 7.3.8
+-- Hôte : localhost
+-- Généré le :  jeu. 20 fév. 2020 à 22:15
+-- Version du serveur :  5.5.64-MariaDB
+-- Version de PHP :  7.0.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+01:00";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `BibliOvox`
+-- Base de données :  `bernar323u`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `audioRec`
+-- Structure de la table `audio`
 --
 
-DROP TABLE IF EXISTS `audioRec`;
+CREATE TABLE `audio` (
+  `idAudio` int(11) NOT NULL,
+  `idU` int(11) NOT NULL,
+  `dateCreation` date NOT NULL,
+  `chemin` varchar(255) NOT NULL,
+  `commentaire` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `audioRec`
+--
+
 CREATE TABLE `audioRec` (
   `idR` int(10) UNSIGNED NOT NULL,
   `idU` int(10) UNSIGNED NOT NULL,
-  `audio` varchar(255) NOT NULL COMMENT 'nom du fichier son'
+  `idAudio` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `audioRec`
+-- Déchargement des données de la table `audioRec`
 --
 
-INSERT INTO `audioRec` (`idR`, `idU`, `audio`) VALUES
-(1, 1, 'demain.mp3');
+INSERT INTO `audioRec` (`idR`, `idU`, `idAudio`) VALUES
+(1, 1, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `classe`
+-- Structure de la table `classe`
 --
 
-DROP TABLE IF EXISTS `classe`;
 CREATE TABLE `classe` (
   `idC` int(10) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL,
@@ -51,36 +71,34 @@ CREATE TABLE `classe` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dicoContient`
+-- Structure de la table `dicoContient`
 --
 
-DROP TABLE IF EXISTS `dicoContient`;
 CREATE TABLE `dicoContient` (
   `idD` int(10) UNSIGNED NOT NULL,
   `idM` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dicoContient`
+-- Déchargement des données de la table `dicoContient`
 --
 
 INSERT INTO `dicoContient` (`idD`, `idM`) VALUES
 (1, 1),
-(2, 1),
-(2, 2),
 (1, 3),
 (1, 4),
-(2, 5),
 (1, 6),
+(2, 1),
+(2, 2),
+(2, 5),
 (2, 6);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dictionnaire`
+-- Structure de la table `dictionnaire`
 --
 
-DROP TABLE IF EXISTS `dictionnaire`;
 CREATE TABLE `dictionnaire` (
   `idD` int(10) UNSIGNED NOT NULL,
   `nomD` varchar(255) NOT NULL,
@@ -89,7 +107,7 @@ CREATE TABLE `dictionnaire` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `dictionnaire`
+-- Déchargement des données de la table `dictionnaire`
 --
 
 INSERT INTO `dictionnaire` (`idD`, `nomD`, `descriptionD`, `imageD`) VALUES
@@ -99,10 +117,9 @@ INSERT INTO `dictionnaire` (`idD`, `nomD`, `descriptionD`, `imageD`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eleve`
+-- Structure de la table `eleve`
 --
 
-DROP TABLE IF EXISTS `eleve`;
 CREATE TABLE `eleve` (
   `idC` int(10) UNSIGNED NOT NULL,
   `idU` int(10) UNSIGNED NOT NULL
@@ -111,17 +128,16 @@ CREATE TABLE `eleve` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `grade`
+-- Structure de la table `grade`
 --
 
-DROP TABLE IF EXISTS `grade`;
 CREATE TABLE `grade` (
   `idG` int(1) UNSIGNED NOT NULL,
   `type` varchar(15) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `grade`
+-- Déchargement des données de la table `grade`
 --
 
 INSERT INTO `grade` (`idG`, `type`) VALUES
@@ -130,73 +146,47 @@ INSERT INTO `grade` (`idG`, `type`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mot`
+-- Structure de la table `mot`
 --
 
-DROP TABLE IF EXISTS `mot`;
 CREATE TABLE `mot` (
   `idM` int(10) UNSIGNED NOT NULL,
   `texte` varchar(255) NOT NULL,
-  `audio` varchar(255) NOT NULL COMMENT 'Nom du fichier son',
+  `idAudio` int(11) NOT NULL COMMENT 'Nom du fichier son',
   `image` varchar(255) NOT NULL COMMENT 'Nom du fichier image'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `mot`
+-- Déchargement des données de la table `mot`
 --
 
-INSERT INTO `mot` (`idM`, `texte`, `audio`, `image`) VALUES
-(1, 'Arbre', 'arbre.mp3', ''),
-(2, 'Bus', 'bus.mp3', ''),
-(3, 'Chateau', 'chateau.mp3', 'chateau.jpg'),
-(4, 'Chevalier', 'chevalier.mp3', ''),
-(5, 'Maison', 'maison.mp3', ''),
-(6, 'Route', 'route.mp3', '');
+INSERT INTO `mot` (`idM`, `texte`, `idAudio`, `image`) VALUES
+(1, 'Arbre', 0, ''),
+(2, 'Bus', 0, ''),
+(3, 'Chateau', 0, 'chateau.jpg'),
+(4, 'Chevalier', 0, ''),
+(5, 'Maison', 0, ''),
+(6, 'Route', 0, '');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `motPerso`
+-- Structure de la table `production`
 --
 
-DROP TABLE IF EXISTS `motPerso`;
-CREATE TABLE `motPerso` (
-  `idU` int(10) UNSIGNED NOT NULL,
-  `idM` int(10) UNSIGNED NOT NULL,
-  `audio` varchar(255) NOT NULL COMMENT 'Nom du fichier son',
-  `commentaire` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `production`
---
-
-DROP TABLE IF EXISTS `production`;
 CREATE TABLE `production` (
-  `idP` int(10) UNSIGNED NOT NULL,
+  `idP` int(11) NOT NULL,
+  `idAudio` int(11) NOT NULL,
   `nomP` varchar(255) NOT NULL,
-  `idU` int(10) UNSIGNED NOT NULL,
-  `commentaire` text,
-  `dateP` date NOT NULL,
-  `audio` varchar(255) NOT NULL COMMENT 'Nom du fichier son'
+  `dateP` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `production`
---
-
-INSERT INTO `production` (`idP`, `nomP`, `idU`, `commentaire`, `dateP`, `audio`) VALUES
-(1, 'Prononciation de \"pizza\"', 1, 'Bonne prononciation !\r\nBravo :)', '2020-01-09', 'rec-1-lucas.mp3');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `recueil`
+-- Structure de la table `recueil`
 --
 
-DROP TABLE IF EXISTS `recueil`;
 CREATE TABLE `recueil` (
   `idR` int(10) UNSIGNED NOT NULL,
   `nomR` varchar(255) NOT NULL,
@@ -205,7 +195,7 @@ CREATE TABLE `recueil` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `recueil`
+-- Déchargement des données de la table `recueil`
 --
 
 INSERT INTO `recueil` (`idR`, `nomR`, `descriptionR`, `dateR`) VALUES
@@ -215,10 +205,9 @@ INSERT INTO `recueil` (`idR`, `nomR`, `descriptionR`, `dateR`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tuteur`
+-- Structure de la table `tuteur`
 --
 
-DROP TABLE IF EXISTS `tuteur`;
 CREATE TABLE `tuteur` (
   `idUEnfant` int(10) UNSIGNED NOT NULL,
   `idUTuteur` int(10) UNSIGNED NOT NULL
@@ -227,10 +216,9 @@ CREATE TABLE `tuteur` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateur`
+-- Structure de la table `utilisateur`
 --
 
-DROP TABLE IF EXISTS `utilisateur`;
 CREATE TABLE `utilisateur` (
   `idU` int(10) UNSIGNED NOT NULL,
   `nom` varchar(255) NOT NULL,
@@ -243,195 +231,196 @@ CREATE TABLE `utilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `utilisateur`
+-- Déchargement des données de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`idU`, `nom`, `prenom`, `password`, `salt`, `mail`, `idG`, `avatar`) VALUES
 (1, 'Sanzey', 'Lucas', 'none', 'none', 'none', 1, 'none');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `audioRec`
+-- Index pour la table `audio`
 --
-ALTER TABLE `audioRec`
-  ADD PRIMARY KEY (`idR`,`idU`),
+ALTER TABLE `audio`
+  ADD PRIMARY KEY (`idAudio`),
   ADD KEY `idU` (`idU`);
 
 --
--- Indexes for table `classe`
+-- Index pour la table `audioRec`
+--
+ALTER TABLE `audioRec`
+  ADD PRIMARY KEY (`idR`,`idU`,`idAudio`),
+  ADD KEY `idU` (`idU`),
+  ADD KEY `idAudio` (`idAudio`),
+  ADD KEY `idR` (`idR`);
+
+--
+-- Index pour la table `classe`
 --
 ALTER TABLE `classe`
   ADD PRIMARY KEY (`idC`),
   ADD KEY `idUEnseignant` (`idUEnseignant`);
 
 --
--- Indexes for table `dicoContient`
+-- Index pour la table `dicoContient`
 --
 ALTER TABLE `dicoContient`
   ADD PRIMARY KEY (`idD`,`idM`),
   ADD KEY `idM` (`idM`);
 
 --
--- Indexes for table `dictionnaire`
+-- Index pour la table `dictionnaire`
 --
 ALTER TABLE `dictionnaire`
   ADD PRIMARY KEY (`idD`);
 
 --
--- Indexes for table `eleve`
+-- Index pour la table `eleve`
 --
 ALTER TABLE `eleve`
   ADD PRIMARY KEY (`idC`,`idU`),
   ADD KEY `idU` (`idU`);
 
 --
--- Indexes for table `grade`
+-- Index pour la table `grade`
 --
 ALTER TABLE `grade`
   ADD PRIMARY KEY (`idG`);
 
 --
--- Indexes for table `mot`
+-- Index pour la table `mot`
 --
 ALTER TABLE `mot`
-  ADD PRIMARY KEY (`idM`);
+  ADD PRIMARY KEY (`idM`,`idAudio`),
+  ADD KEY `idAudio` (`idAudio`);
 
 --
--- Indexes for table `motPerso`
---
-ALTER TABLE `motPerso`
-  ADD PRIMARY KEY (`idU`,`idM`),
-  ADD KEY `idM` (`idM`);
-
---
--- Indexes for table `production`
+-- Index pour la table `production`
 --
 ALTER TABLE `production`
-  ADD PRIMARY KEY (`idP`),
-  ADD KEY `idU` (`idU`);
+  ADD PRIMARY KEY (`idP`,`idAudio`),
+  ADD KEY `idAudio` (`idAudio`);
 
 --
--- Indexes for table `recueil`
+-- Index pour la table `recueil`
 --
 ALTER TABLE `recueil`
   ADD PRIMARY KEY (`idR`);
 
 --
--- Indexes for table `tuteur`
+-- Index pour la table `tuteur`
 --
 ALTER TABLE `tuteur`
   ADD PRIMARY KEY (`idUEnfant`,`idUTuteur`),
   ADD KEY `idUTuteur` (`idUTuteur`);
 
 --
--- Indexes for table `utilisateur`
+-- Index pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD PRIMARY KEY (`idU`),
   ADD KEY `idG` (`idG`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `classe`
+-- AUTO_INCREMENT pour la table `audio`
+--
+ALTER TABLE `audio`
+  MODIFY `idAudio` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `classe`
 --
 ALTER TABLE `classe`
   MODIFY `idC` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `dictionnaire`
+-- AUTO_INCREMENT pour la table `dictionnaire`
 --
 ALTER TABLE `dictionnaire`
   MODIFY `idD` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `eleve`
+-- AUTO_INCREMENT pour la table `eleve`
 --
 ALTER TABLE `eleve`
   MODIFY `idC` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `mot`
+-- AUTO_INCREMENT pour la table `mot`
 --
 ALTER TABLE `mot`
   MODIFY `idM` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `production`
+-- AUTO_INCREMENT pour la table `production`
 --
 ALTER TABLE `production`
-  MODIFY `idP` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idP` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `recueil`
+-- AUTO_INCREMENT pour la table `recueil`
 --
 ALTER TABLE `recueil`
   MODIFY `idR` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `utilisateur`
+-- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   MODIFY `idU` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `audioRec`
+-- Contraintes pour la table `audioRec`
 --
 ALTER TABLE `audioRec`
   ADD CONSTRAINT `audiorec_ibfk_1` FOREIGN KEY (`idR`) REFERENCES `recueil` (`idR`),
   ADD CONSTRAINT `audiorec_ibfk_2` FOREIGN KEY (`idU`) REFERENCES `utilisateur` (`idU`);
 
 --
--- Constraints for table `classe`
+-- Contraintes pour la table `classe`
 --
 ALTER TABLE `classe`
   ADD CONSTRAINT `classe_ibfk_1` FOREIGN KEY (`idUEnseignant`) REFERENCES `utilisateur` (`idU`);
 
 --
--- Constraints for table `dicoContient`
+-- Contraintes pour la table `dicoContient`
 --
 ALTER TABLE `dicoContient`
   ADD CONSTRAINT `dicocontient_ibfk_1` FOREIGN KEY (`idD`) REFERENCES `dictionnaire` (`idD`),
   ADD CONSTRAINT `dicocontient_ibfk_2` FOREIGN KEY (`idM`) REFERENCES `mot` (`idM`);
 
 --
--- Constraints for table `eleve`
+-- Contraintes pour la table `eleve`
 --
 ALTER TABLE `eleve`
   ADD CONSTRAINT `eleve_ibfk_1` FOREIGN KEY (`idU`) REFERENCES `utilisateur` (`idU`),
   ADD CONSTRAINT `eleve_ibfk_2` FOREIGN KEY (`idC`) REFERENCES `classe` (`idC`);
 
 --
--- Constraints for table `motPerso`
---
-ALTER TABLE `motPerso`
-  ADD CONSTRAINT `motperso_ibfk_1` FOREIGN KEY (`idU`) REFERENCES `utilisateur` (`idU`),
-  ADD CONSTRAINT `motperso_ibfk_2` FOREIGN KEY (`idM`) REFERENCES `mot` (`idM`);
-
---
--- Constraints for table `production`
---
-ALTER TABLE `production`
-  ADD CONSTRAINT `production_ibfk_1` FOREIGN KEY (`idU`) REFERENCES `utilisateur` (`idU`);
-
---
--- Constraints for table `tuteur`
+-- Contraintes pour la table `tuteur`
 --
 ALTER TABLE `tuteur`
   ADD CONSTRAINT `tuteur_ibfk_1` FOREIGN KEY (`idUEnfant`) REFERENCES `utilisateur` (`idU`),
   ADD CONSTRAINT `tuteur_ibfk_2` FOREIGN KEY (`idUTuteur`) REFERENCES `utilisateur` (`idU`);
 
 --
--- Constraints for table `utilisateur`
+-- Contraintes pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
   ADD CONSTRAINT `utilisateur_ibfk_1` FOREIGN KEY (`idG`) REFERENCES `grade` (`idG`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
