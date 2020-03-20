b82ea5fa514204d5af682f343181835efd98d9a3
claude216u <paul.claude7@etu.univ-lorraine.fr>
2020-03-20 11:48:13 +0100

    Ajout de la méthode recherchant les recueils partagés

--- src/controllers/ControleurAudioRecueil.php
+++ src/controllers/ControleurAudioRecueil.php
@@ -34,4 +34,29 @@ class ControleurAudioRecueil
         return $ret;
     }
 
+    static function audioPartage (int $idR) : string {
+        $ret = "";
+        $ar = AudioRecueil::where("idR", "=", "$idR")->where("partage", "=", "1")->get();
+
+        foreach ($ar as $row){
+            $aud = ControleurAudio::getPathById($row->idAudio);
+
+            $date = explode('-', $aud->dateCreation);
+            $an = $date[0];
+            $mois = $date[1];
+            $jour = explode(" ", $date[2])[0];
+
+            $ret .= "<p>Exemple de <b>" . ControleurUtilisateur::getNameById($aud->idU) . "</b></p>";
+            $ret .= "<p class='date'>Créé le: $jour / $mois / $an</p>";
+            $ret .= "<audio controls>";
+            $ret .= "<source src=' " . $GLOBALS["PATH"] . "/" . $aud->chemin . "' type='audio/mp3'>";
+            $ret .= "</audio>";
+        }
+
+        if ($ret == "")
+            $ret = "<p>Aucun exemple pour l'instant.</p><p>Tu peux demander à ta maitresse ou à ton maitre de te choisir comme exemple !</p>";
+
+        return $ret;
+    }
+
 }
