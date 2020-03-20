50fd8a34c66dd06fdc23cb723fa2ff6c93931d2b
claude216u <paul.claude7@etu.univ-lorraine.fr>
2020-03-20 11:31:52 +0100

    Création d'un méthode de recherche des audios perso pour les recueuils

--- src/controllers/ControleurAudioRecueil.php
+++ src/controllers/ControleurAudioRecueil.php
@@ -11,17 +11,27 @@ use bibliovox\models\AudioRecueil;
 class ControleurAudioRecueil
 {
 
-    static function renderAudio (int $idR, int $idU) {
-        $ar = AudioRecueil::getByBoth($idR, $idU);
-        if ($ar != null){
-            echo "<div class='comm'>Ton enregistrement: </div>";
-            echo "<audio controls>";
-            echo "<source src='" . PATH . "/media/aud/rec/" . $ar->audio . "' type='audio/mp3'>";
-            echo "</audio></div>";
-        } else {
-            echo "<div class='comm'>Enregistre toi ! </div>";
+    static function audioPerso (int $idR, int $idU) : string {
+        $ret = "";
+        $ar = AudioRecueil::where("idR", "=", "$idR")->where("idU", "=", "$idU")->get();
+        foreach ($ar as $row){
+            $aud = ControleurAudio::getPathById($row->idAudio);
+
+            $date = explode('-', $aud->dateCreation);
+            $an = $date[0];
+            $mois = $date[1];
+            $jour = explode(" ", $date[2])[0];
+
+            $ret .= "<p class='date'>Créé le: $jour / $mois / $an</p>";
+            $ret .= "<audio controls>";
+            $ret .= "<source src=' " . $GLOBALS["PATH"] . "/" . $aud->chemin . "' type='audio/mp3'>";
+            $ret .= "</audio>";
         }
 
+        if ($ret == "")
+            $ret = "<p>Tu n'as pas encore d'enregistrement.</p><p>Enregistre-toi tout de suite !</p>";
+
+        return $ret;
     }
 
 }
